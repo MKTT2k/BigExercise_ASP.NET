ff579bbd34a2b370b74331c3fb63be8b8bc33180
MKTT2k <63504561+MKTT2k@users.noreply.github.com>
2022-01-01 08:48:29 +0700

    work with payment

--- BTL_ASP_Nhom13/BTL_ASP_HieuHaiSan/Controllers/GioHangController.cs
+++ BTL_ASP_Nhom13/BTL_ASP_HieuHaiSan/Controllers/GioHangController.cs
@@ -1,4 +1,5 @@
-﻿using BTL_ASP_HieuHaiSan.Models;
+﻿using BTL_ASP_HieuHaiSan.Common;
+using BTL_ASP_HieuHaiSan.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -79,8 +80,9 @@ namespace BTL_ASP_HieuHaiSan.Controllers
             {
                 return 0;
             }
-            return lstGioHang.Sum(n => n.ThanhTien);
+            return lstGioHang.Sum(n => n.SoLuong * n.DonGia);
         }
+
         public ActionResult XemGioHang()
         {
             List<ItemGioHang> lstGioHang = LayGioHang();
@@ -126,12 +128,94 @@ namespace BTL_ASP_HieuHaiSan.Controllers
             return RedirectToAction("XemGioHang");
         }
 
+        public ActionResult CapNhatGioHang(int MaSP, FormCollection f)
+        {
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //ViewBag.TongSoLuong = TinhTongSoLuong();
+            //ViewBag.TongTien = TinhTongTien();
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(n => n.ID_SanPham == MaSP);
+            if (spCheck != null)
+            {
+                spCheck.SoLuong = int.Parse(f["quantity"].ToString());
+            }
+            return RedirectToAction("XemGioHang");
+        }
+
         public ActionResult ThanhToan()
         {
             List<ItemGioHang> lstGioHang = LayGioHang();
-            ViewBag.TongSoLuong = TinhTongSoLuong();
             ViewBag.TongTien = TinhTongTien();
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //TaiKhoan tk = new TaiKhoan();
+            //if (Session[BTL_ASP_HieuHaiSan.Common.CommonConstants.USER_SESSION] == null)
+            //{
+            //    tk = t;
+            //    db.TaiKhoans.Add(tk);
+            //    db.SaveChanges();
+            //}
+            //else
+            //{
+            //    TaiKhoan acc = Session[BTL_ASP_HieuHaiSan.Common.CommonConstants.USER_SESSION] as TaiKhoan;
+            //    tk.HoTen = acc.HoTen;
+            //    tk.GioiTinh = acc.GioiTinh;
+            //    tk.DiaChi = acc.DiaChi;
+            //    tk.Email = acc.Email;
+            //    tk.SDT = acc.SDT;
+            //    tk.TrangThai = acc.TrangThai;
+            //    tk.MatKhau = acc.MatKhau;
+            //    db.TaiKhoans.Add(tk);
+            //    db.SaveChanges();
+
+            //}
             return View(lstGioHang);
+
+        }
+
+        public ActionResult DatHang(FormCollection f)
+        {
+            DonHang dh = new DonHang();
+            TaiKhoanDangNhap acc = (TaiKhoanDangNhap)Session[BTL_ASP_HieuHaiSan.Common.CommonConstants.USER_SESSION];
+
+            List<ItemGioHang> lstGioHang = LayGioHang();
+
+            dh.ID_TaiKhoan = acc.ID_TaiKhoan;
+            dh.NguoiNhan = f["txt_cfullname"].ToString();
+            dh.SDTNhan = f["txt_nphone"].ToString();
+            dh.DiaChiNhan = f["txt_caddress"].ToString();
+            dh.EmailNhan = f["txt_cemail"].ToString();
+            dh.GhiChu = f["txt_cnote"].ToString();
+            int x = int.Parse(f["type_payment"].ToString());
+            if (x == 1)
+            {
+                dh.PTThanhToan = true;
+            }
+            else
+            {
+                dh.PTThanhToan = false;
+            }
+            db.DonHangs.Add(dh);
+            db.SaveChanges();
+            foreach (var item in lstGioHang)
+            {
+                ChiTietDonHang ctdh = new ChiTietDonHang();
+                ctdh.ID_DonHang = dh.ID_DonHang;
+                ctdh.ID_SanPham = item.ID_SanPham;
+                ctdh.SoLuongMua = item.SoLuong;
+                db.ChiTietDonHangs.Add(ctdh);
+
+            }
+            db.SaveChanges();
+            Session["GioHang"] = null;
+
+            return RedirectToAction("XacNhanDonHang", "GioHang");
+        }
+
+        public ActionResult XacNhanDonHang()
+        {
+            return View();
         }
     }
 }
